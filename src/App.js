3223efaf76e3d75d93369c005a5d31a510a702f7
Jorge E Romero <jorgee.romero@iMac-de-Jorge.local>
2023-05-16 15:49:28 -0500

    flow4

--- src/App.js
+++ src/App.js
@@ -65,6 +65,8 @@ function App() {
  
              <h1>forty and fabulous</h1>
 
+             <div className='carito'></div>
+
              <div className='message'> YOU ARE INVITED TO CELEBRATE MY 40TH BIRTHDAY </div>
 
              <div className='caro'>Diana </div>
